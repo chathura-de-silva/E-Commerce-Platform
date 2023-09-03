edaf139cba126606319b1f9f3a625f4bbfa528ff
Chathura De Silva <97898278+chathura7357@users.noreply.github.com>
2023-09-04 05:01:07 +0530

    Database generation basics implemented when doesnt exist

--- Backend/databaseConfig.py
+++ Backend/databaseConfig.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import errorcode
 
 
 def database_connector():
@@ -8,19 +9,35 @@ def database_connector():
         "user": "root",
         "password": "",
         "database": "lab_2",
-        "raise_on_warnings" : True  #Thows an exception when there is an error with other provided parameters.
+        "raise_on_warnings": True
+        # Throws an exception when there is an error with other provided parameters such as when database does not
+        # exist.
     }
 
-    # config["host"] = input("Enter your MySQL Hostname (default: localhost): ")
-    # config["user"] = input("Enter your MySQL Username (default: root): ")
+    # _config["host"] = input("Enter your MySQL Hostname (default: localhost): ")
+    # _config["user"] = input("Enter your MySQL Username (default: root): ")
     _config["password"] = input("Enter your MySQL Password: ")
-    # config["database"] = input("Enter the name of the MySQL Database: ")
+    # _config["database"] = input("Enter the name of the MySQL Database: ")
 
     # Creating a connection to the MySQL server
     try:
         connection = mysql.connector.connect(**_config)
-        print("Successfully Connected to the MYSQL Database")
+        print(f'Successfully Connected to the MYSQL Database "{_config["database"]}".')
         return connection
     except mysql.connector.Error as err:
-        print(f"Database Server Connection Failed!\nError: {err}")
-        return 0
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database not Found! Generating the initial database")
+            return generate_database(_config)
+        else:
+            print(f"Database Server Connection Failed!\nError: {err}")
+            return 0
+
+
+def generate_database(config):
+    config.pop("raise_on_warnings")
+    db_name = config.pop("database")
+    connection = mysql.connector.connect(**config)  # No try catch block added since only error that reach here is
+                                                     # database doesn't exist.
+    connection.cursor().execute(f'CREATE DATABASE {db_name}')
+    print(f'Database Generated as "{db_name}".')
+    return connection
