752c6934231c1b61fc27e7294483458bbaadc583
chathura-de-silva <ac.chathura@gmail.com>
2023-11-03 10:54:59 +0530

    Adding procedure queries to .sql file

--- webapp/dbInitialData/database_relations.sql
+++ webapp/dbInitialData/database_relations.sql
@@ -144,3 +144,79 @@ ALTER TABLE delivery_module
 ADD CONSTRAINT fk_delivery_module
 FOREIGN KEY (order_item_id)
 REFERENCES  order_item(order_item_id);
+
+/*
+procedures
+*/
+
+CREATE PROCEDURE AddUser(
+  IN p_email VARCHAR(255),
+  IN p_password VARCHAR(255),
+  IN p_username VARCHAR(255)
+)
+BEGIN
+  INSERT INTO registered_user (email, password, username)
+  VALUES (p_email, p_password, p_username);
+END;
+
+CREATE PROCEDURE get_cart(IN user_id INT)
+BEGIN
+    SELECT
+        ci.quantity AS quantity,
+        v.name AS name,
+        v.price AS price,
+        v.variant_image AS variant_image,
+        p.title AS title,
+        v.variant_id AS variant_id
+    FROM
+        cart_item AS ci
+    JOIN
+        variant AS v ON ci.variant_id = v.variant_id
+    JOIN
+        product AS p ON v.product_id = p.product_id
+    WHERE
+        ci.user_id = user_id;
+END;
+
+CREATE PROCEDURE get_categories(IN parent_category_name VARCHAR(255))
+BEGIN
+    SELECT Category.category_name, Category.category_image, Category.category_id
+    FROM Category
+    WHERE Category.parent_category_id = (
+        SELECT category_id FROM Category WHERE category_name = parent_category_name
+    );
+END;
+
+CREATE PROCEDURE get_guest_cart(IN variant_id INT)
+BEGIN
+    SELECT p.title, v.name, v.price, v.variant_image, v.variant_id
+    FROM product AS p
+    JOIN variant AS v ON p.product_id = v.product_id
+    WHERE v.variant_id = variant_id;
+END;
+
+CREATE PROCEDURE get_variant_name(IN order_item_id INT)
+BEGIN
+    SELECT variant.name AS variant_name, delivery_module.estimated_days AS estimated_days
+    FROM order_item
+    INNER JOIN variant ON order_item.variant_id = variant.variant_id
+    LEFT JOIN delivery_module ON order_item.order_item_id = delivery_module.order_item_id
+    WHERE order_item.order_item_id = order_item_id;
+END;
+
+CREATE PROCEDURE GetOrderItemDetails(IN order_item_id INT)
+BEGIN
+    SELECT variant.name, delivery_module.estimated_days
+    FROM order_item
+    INNER JOIN variant ON order_item.variant_id = variant.variant_id
+    LEFT JOIN delivery_module ON order_item.order_item_id = delivery_module.order_item_id
+    WHERE order_item.order_item_id = order_item_id;
+END;
+
+CREATE PROCEDURE set_stock_count_NULL(IN variant_id INT)
+BEGIN
+    -- Update the stock count to zero if it's less than zero for the specified variant ID
+    UPDATE inventory
+    SET stock_count = 0
+    WHERE variant_id = variant_id AND stock_count < 0;
+END;
